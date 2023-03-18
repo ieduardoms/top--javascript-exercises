d993b337e7266dbeb5d264a27d199b3e3ab4578c
Eduardo Moreno <git config --global user.email>
2023-03-18 22:55:13 +0800

    Solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function (string, num) {
+    vsText = "";
+    string = string.trim();
+    if (string == "") {
+        return "";
+    }
+    if (num >= 0) {
+        for (i = 0; i < num; i++) {
+            vsText += "hey";
+        }
+    } else {
+        vsText = "ERROR";
+    }
+    return vsText;
+}
 // Do not edit below this line
 module.exports = repeatString;

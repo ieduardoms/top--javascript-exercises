5ac382825fc028238e9295ea6e4b7c2ac4dcc8bb
Eduardo Moreno <git config --global user.email>
2023-03-20 14:46:02 +0800

    Solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...deleteElements) {
+    for (let element of deleteElements){
+        let pos = inputArray.indexOf(element);
+        if (pos >= 0){
+            inputArray.splice(pos, 1);
+        }
+    }
+    return inputArray;
 };
 
 // Do not edit below this line

5387e19cb87da2ab0b44f69f46685f94eda9b504
Eduardo Moreno <git config --global user.email>
2023-03-22 12:21:18 +0800

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0) return "ERROR";
+    if (min > max) {
+        const tmpNumber = min;
+        min = max;
+        max = tmpNumber;
+    }
+    let sum = 0;
+    do {
+        sum += min;
+        min++;
+    } while (min <= max);
+    return sum;
 };
+
+//console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;

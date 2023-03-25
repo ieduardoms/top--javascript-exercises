ca0fead7f5505da73ac7721ee0915d0007836d08
Eduardo Moreno <git config --global user.email>
2023-03-25 11:23:32 +0800

    Solve 10_fibonacci

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let sum = 0
+  numbers.forEach(number => sum += number)
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let result = 1
+  numbers.forEach(number => result *= number)
+  return result
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0) return 1
+  result = 1
+	for (let i = number; i > 0; i--){
+    result *= i
+  }
+  return result
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (numberTh) {
+    if (typeof numberTh === "string") numberTh = parseInt(numberTh)
+    if (numberTh < 0) return "OOPS"
+    a = 0
+    b = 1
+    i = 0
+    do {
+        i++
+        fNumber = a + b
+        a = b
+        b = fNumber
+    } while (i < numberTh - 1)
+    return fNumber
+    //1, 1, 2, 3, 5, 8,
 };
 
 // Do not edit below this line

4e722fa6cd4997449486ddde0d1a10dbd14bde27
Eduardo Moreno <git config --global user.email>
2023-04-09 11:36:23 +0800

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,42 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    let now = new Date()
+    return people.reduce((person1, person2) => {
+        let person1Age = 0
+        let person2Age = 0
+        if (person1.yearOfDeath !== undefined){
+            person1Age = person1.yearOfDeath - person1.yearOfBirth
+        } else {
+            person1Age = now.getFullYear() - person1.yearOfBirth
+        }
+        if (person2.yearOfDeath !== undefined){
+            person2Age = person2.yearOfDeath - person2.yearOfBirth
+        } else {
+            person2Age = now.getFullYear() - person2.yearOfBirth
+        }
+        if(person1Age > person2Age){
+            return person1
+        } else {
+            return person2
+        }
+    })
 };
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1066,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    }
+  ]
+  console.log(findTheOldest(people))
 
 // Do not edit below this line
 module.exports = findTheOldest;
--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,27 @@
-const caesar = function() {
+const caesar = function (text, num) {
+    return text
+        .split("")
+        .map(letter => shiftLetter(letter, num))
+        .join("")
+}
 
-};
+const codeSet = (code) => (code < 97 ? 65 : 97);
+
+// this function is just a fancy way of doing % so that it works with negative numbers
+// see this link for details:
+// https://stackoverflow.com/questions/4467539/javascript-modulo-gives-a-negative-result-for-negative-numbers
+const mod = (n, m) => ((n % m) + m) % m;
+
+function shiftLetter(letter, num) {
+    const code = letter.charCodeAt();
+
+    if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
+      return String.fromCharCode(
+        mod(code + num - codeSet(code), 26) + codeSet(code)
+      );
+    }
+    return letter;
+}
 
 // Do not edit below this line
 module.exports = caesar;
